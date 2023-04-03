5baa30cc545701de6a99a439808b5628765e439e
Saleryv <seryoginvaleriy@gmail.com>
2023-04-03 17:48:49 +0300

    hw-05

--- controllers/contacts/updateContactById.js
+++ controllers/contacts/updateContactById.js
@@ -1,14 +1,15 @@
-const {Contact} = require("../../models/contact")
+const { Contact } = require("../../models/contact");
 
-const {RequestError} = require("../../helpers")
+const { RequestError } = require("../../helpers");
 
-const updateContactById = async(req, res)=> {
-    const {id} = req.params;
-    const result = await Contact.findByIdAndUpdate(id, req.body, {new: true});
-    if(!result){
-        throw RequestError(404, "Not found")
-    }
-    res.status(201).json(result)
-}
+const updateContactById = async (req, res) => {
+  const { id } = req.params;
+  const { _id: owner } = req.user;
+  const result = await Contact.findByIdAndUpdate(id, req.body, { new: true }, owner);
+  if (!result) {
+    throw RequestError(404, "Not found");
+  }
+  res.status(201).json(result);
+};
 
 module.exports = updateContactById;
